496fea8cd11a0b093c6cc75ebe4242beb8dff0f0
williamforbrigd <hansw0701@gmail.com>
2021-03-18 20:41:13 +0100

    mapped address

--- stun/src/message/StunMessage.java
+++ stun/src/message/StunMessage.java
@@ -2,7 +2,22 @@ package message;
 
 /**
  * Class for message.StunMessage
+ *
+ *
+ *  0                   1                   2                   3
+ *        0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *       |0 0|     STUN Message Type     |         Message Length        |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *       |                         Magic Cookie                          |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ *       |                                                               |
+ *       |                     Transaction ID (96 bits)                  |
+ *       |                                                               |
+ *       +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
  */
+
+
 public class StunMessage {
     private int messageType;
     private int messageLength;
@@ -11,9 +26,10 @@ public class StunMessage {
     private MessageTypeClass messageTypeClass;
 
     public static final int BUFFER_LENGTH = 160;
+    private byte[] buffer;
 
 
-    private enum MessageTypeClass {BINDING_REQUEST, INDICATION, SUCCESS_RESPONSE, ERROR_RESPONSE};
+    public enum MessageTypeClass {BINDING_REQUEST, INDICATION, SUCCESS_RESPONSE, ERROR_RESPONSE};
 
     /**
      * TODO: check that the 2 first bytes are 0 and also check that the other attributes are of correct byte length.
@@ -26,7 +42,19 @@ public class StunMessage {
         this.messageLength = messageLength;
     }
 
+    public StunMessage(byte[] buffer) {
+        this.buffer = buffer;
+    }
+
+    public StunMessage(MessageTypeClass messageTypeClass) {
+        this.messageTypeClass = messageTypeClass;
+    }
+
     public StunMessage() {}
+
+    public MessageTypeClass getMessageTypeClass() {
+        return this.messageTypeClass;
+    }
 
     public MessageTypeClass findMessageType(int messageType) {
         if((messageType & 0x0110) == 0x0000) return MessageTypeClass.BINDING_REQUEST;
@@ -51,5 +79,23 @@ public class StunMessage {
             i++;
         }
         return str;
+    }
+
+    //TODO: parse the bytes from a buffer and get the message type
+    public static StunMessage checkMessage(byte[] buffer) {
+        return new StunMessage(MessageTypeClass.BINDING_REQUEST);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(2 >> 1);
+        System.out.println(2 >> 2);
+        System.out.println(2 >> 3);
+        System.out.println(2 << 1);
+        System.out.println(2 << 2);
+        System.out.println(2 << 3);
+        int num = Integer.parseInt("1011", 2);
+        System.out.println(num >> 1);
+        System.out.println(num);
+        System.out.println(Integer.parseInt("1011", 2));
     }
 }
--- stun/src/server/StunServer.java
+++ stun/src/server/StunServer.java
@@ -1,7 +1,9 @@
 package server;
 
 import message.StunMessage;
+import stunattributes.XorMappedAddress;
 
+import java.awt.*;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -9,25 +11,36 @@ import java.net.InetAddress;
 import java.net.SocketException;
 
 public class StunServer {
-    private static final int PORT = 1252;
     private InetAddress address;
     private DatagramSocket socket;
     private int bufferLength;
+    private byte[] buffer;
+    private ICEServer iceServer;
 
-    public StunServer(int bufferLength) throws SocketException {
+    public StunServer(int bufferLength, String stunServer, int defaultPort) throws SocketException {
         this.bufferLength = bufferLength;
-        socket = new DatagramSocket(1252);
+        socket = new DatagramSocket();
+        iceServer = new ICEServer(stunServer, defaultPort);
     }
 
     public void start() throws IOException {
-        byte[] buffer = new byte[this.bufferLength];
+        socket.connect(InetAddress.getByName(this.iceServer.getStunServer()), this.iceServer.getDefaultPort());
+        //Receiving a STUN message
+        buffer = new byte[this.bufferLength];
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
         socket.receive(packet);
+        StunMessage msg = StunMessage.checkMessage(packet.getData());
         System.out.println(StunMessage.byteToString(packet.getData()));
+
+        if(msg.getMessageTypeClass() == StunMessage.MessageTypeClass.BINDING_REQUEST) {
+            XorMappedAddress xorMappedAddress = new XorMappedAddress();
+
+        }
     }
 
     public static void main(String[] args) throws IOException {
-        StunServer server = new StunServer(StunMessage.BUFFER_LENGTH);
+        //The default port for TCP and UDP are 3478
+        StunServer server = new StunServer(StunMessage.BUFFER_LENGTH, "jstun.javawi.de", 3478);
         server.start();
     }
 
