31f7dc05ccf8e14e68bc293224a88331c236cbe7
williamforbrigd <hansw0701@gmail.com>
2021-03-24 15:37:03 +0100

    Added jar files.

---
+++ stun-server/run.sh
@@ -0,0 +1,5 @@
+java -jar ./out/artifacts/stun_server_jar/stun_server.jar &
+java -jar ./out/artifacts/public_nat_jar/stun_server.jar &
+java -jar ./out/artifacts/private_nat_jar/stun_server.jar &
+java -jar ./out/artifacts/stun_client_jar/stun_server.jar
+
